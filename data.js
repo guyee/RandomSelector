abdcf85ab2b9b57dcc6ad9fac6dadd1633eefb53
Tikhon Botchkarev <TikiTDO@gmail.com>
2013-05-06 18:10:47 -0400

    Include data from a separate file

---
+++ data.js
@@ -0,0 +1,10 @@
+thing_inst = container.generate("Canyon river snakes Barely kissing the soft shoulders of the hovering hills")
+thing_inst.generate("Substuff")
+thing_inst.generate("Substuff2")
+
+myLines[1] = "Canyon river snakes Barely kissing the soft shoulders of the hovering hills"
+myLines[2] = "You pour yourself out Spilling forth your entirety All on our behalf "
+myLines[3] = "Her face stroked the years with broken sadnessand slightly chilled the guilty hands limb bare, shrunken, shuddered time good-bye "
+myLines[4] = "Now placid vistas belie the brutal carving of there creation"
+myLines[5] = "Lines once scrawled in haste leave there closeted intentions to reach prominence"
+myLines[6] = "This is a test of the emergency broadcasting system  if it were not a test you would be dead by now"
